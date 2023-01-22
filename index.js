c084b6fb59949bfbed746f69255b94cc9958164e
Shivam Kumar <kumarshivam16062004@gmail.com>
2023-01-22 12:35:04 +0530

    version 1.0

---
+++ index.js
@@ -0,0 +1,13 @@
+
+function clicked(){
+    alert("This service is not currently available")
+}
+function clicke0(){
+    alert("are you a traveller")
+}
+function clicke1(){
+    alert("go out at a low cost")
+}
+function clicke2(){
+    alert("in india cool decision")
+}
